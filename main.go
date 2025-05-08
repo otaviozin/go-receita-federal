6680db78c8df1e3382a4dc9d7a6e202e6f06e0e8
Otávio <otaviopereira2004@gmail.com>
2025-05-07 22:36:42 -0300

    add: docker added with postgres database

--- main.go
+++ main.go
@@ -1,5 +1,19 @@
 package main
 
+import (
+	"log"
+
+	"github.com/otaviozin/go-receita-federal/database"
+	"github.com/otaviozin/go-receita-federal/types"
+)
+
 func main() {
-	
+	db := database.DBConnection()
+
+	err := db.AutoMigrate(&types.Empresa{})
+	if err != nil {
+		log.Fatalf("Erro ao fazer AutoMigrate: %v", err)
+	}
+
+	log.Println("Banco de dados migrado com sucesso!")
 }
